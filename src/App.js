d40ff19fe02a8fafb783be330602f354ec00e1f3
Martin Jaere <martin@jaere.no>
2018-04-05 14:42:53 +0100

    Use React Component class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import paintingList from "./paintingList.js"
+import paintingList from "./paintingList.js";
 
-const App = () => {
-  return (
-    <div>
-      <div className="leftPanel">
-        {" "}
-        <div className="header">Kate Warner</div>
-        <ul className="menulist">
-          <li className={["menuitem", "active"]}>
-            <a href="http://katewarner.net/">Main</a>
-          </li>
-        </ul>
-        <ul className="menulist">
-          <li className={["menuitem", "active"]}>
-            <a href="http://katewarner.net/">Info</a>
-          </li>
-          <li className={["menuitem", "active"]}>
-            <a href="http://katewarner.net/">News/Contact</a>
-          </li>
-        </ul>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activePage: "Main" };
+  }
+
+  changePage(pageName) {
+    console.log(pageName);
+    this.setState({ activePage: pageName });
+
+  }
+
+  render() {
+    const isMainActive = this.state.activePage == "Main";
+    const isContactActive = this.state.activePage == "Contact";
+    const isInfoActive = this.state.activePage == "Info";
+    return (
+      <div>
+        <div className="leftPanel">
+          {" "}
+          <div className="header">Kate Warner</div>
+          <ul className="menulist">
+            <li
+              className={["menuitem", isMainActive ? "active" : ""].join(
+                " "
+              )}
+            >
+              <a href="#" onClick={() => this.changePage("Main")}>
+                Main
+              </a>
+            </li>
+          </ul>
+          <ul className="menulist">
+            <li
+              className={["menuitem",  isInfoActive ? "active" : ""].join(
+                " "
+              )}
+            >
+              <a href="#" onClick={() => this.changePage("Info")}>
+                Info
+              </a>
+            </li>
+            <li
+              className={["menuitem", isContactActive ? "active" : ""].join(
+                " "
+              )}
+            >
+              <a href="#" onClick={() => this.changePage("Contact")}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <ul className="menulist">
+            <li>copyright 2018</li>
+          </ul>
+        </div>
+        <div className="rightPanel">{paintingList}</div>
       </div>
-      <div className="rightPanel">{paintingList}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
 ReactDOM.render(<App />, document.getElementById("app"));
