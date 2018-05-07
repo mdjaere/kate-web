f13233e368242211086ddfbe474e2d46b82ba4b5
Martin D. Jaere <mdjaere@wssu-mj2911a.cx.med.ic.ac.uk>
2018-05-07 23:47:24 +0100

    Remove host from server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@
 const express = require("express");
 
 const PORT = 8080;
-const HOST = "0.0.0.0";
 
 const app = express();
 app.use(express.static('dist'))
-app.listen(PORT, HOST);
-console.log(`Running server at ${HOST}:${PORT}`);
+app.listen(PORT);
+console.log(`Running server at ${PORT}`);
