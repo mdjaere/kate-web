efe17df48e1b53fdfb3cf8245b43132f94b77d7b
Martin D. Jaere <martin@jaere.no>
2018-11-02 21:39:00 +0000

    Updating express request

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 "use strict";
+const path = require("path")
 
 const express = require("express");
 
 const PORT = 8080;
 
 const app = express();
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
 app.listen(PORT);
 console.log(`Running server with port ${PORT}`);
