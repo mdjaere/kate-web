fd2401d0aca31e6eb79515b1313d64df4ff0d4d0
Martin D. Jaere <martin@jaere.no>
2018-12-21 21:42:45 +0000

    Add back static to express server

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 
-// app.get('/*', (req,res) =>{
-//     res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "dist/index.html"), function(err) {
